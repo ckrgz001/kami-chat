6bdf1acdfa4bb590f6dc8f8733eedda2f4429d17
ckrgz001 <ckrgz001@outlook.com>
2017-09-29 19:48:43 -0400

    19:48

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,24 @@
 (function() {
-    function HomeCtrl(Room) {
+    function HomeCtrl(Room,$uibModal) {
         this.rooms = Room.all;
-    }
+
+
+        this.openRoomModal = function(){
+            var modalInstance = $uibModal.open({
+                templateUrl: "/templates/roommodal.html",
+                size: 'lg',
+                controller: 'ModalCtrl as modal',
+                backdrop:true,
+            });
+
+            modalInstance.result.then(function(newRoomName){
+                Room.add(newRoomName);
+                console.log("hello");
+            })
+        };
+    };
 
     angular
         .module('kamiChat')
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['$uibModal','Room', HomeCtrl]);
 })();
