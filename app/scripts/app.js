0b271610082b774fee1775e303c7e04a3b70dec9
ckrgz001 <ckrgz001@outlook.com>
2017-10-07 20:14:06 -0400

    added index files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,9 +12,20 @@
                 controller: 'HomeCtrl as home',
                 templateUrl: '/templates/home.html'
             });
+
+
+
+    function KamiChatCookies($cookies, $uibModal) {
+            var currentUser = $cookies.get('kamiChatCurrentUser');
+            if (!currentUser || currentUser === '') {
+                $uibModal.open({
+          // Modal configuration object properties
+        })
+      }
     }
 
      angular
-         .module('kamiChat', ['ui.router', 'ui.bootstrap','firebase'])
-         .config(config);
+         .module('kamiChat', ['ui.router', 'ui.bootstrap','firebase', 'ngCookies'])
+         .config(config)
+         .run(['$cookies', '$uibModal', KamiChatCookies]);
  })();
